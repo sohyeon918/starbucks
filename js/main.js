098b1b564cd30707a9f6a073e360d739bca03ac1
sohyeon918 <tysidzlz@gmail.com>
2022-10-06 18:30:04 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,83 +1,47 @@
-'use strict'
+const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
-/**
- * 검색창 제어
- */
-// 검색창 요소(.search) 찾기.
-const searchEl = document.querySelector('.search')
-const searchInputEl = searchEl.querySelector('input')
-// 검색창 요소를 클릭하면 실행.
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus()
-})
-// 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused')
-  searchInputEl.setAttribute('placeholder', '통합검색')
-})
-// 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused')
-  searchInputEl.setAttribute('placeholder', '')
-})
-
-
-/**
- * 페이지 스크롤에 따른 요소 제어
- */
-// 페이지 스크롤에 영향을 받는 요소들을 검색!
-const badgeEl = document.querySelector('header .badges')
-const toTopEl = document.querySelector('#to-top')
-// 페이지에 스크롤 이벤트를 추가!
-// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
 window.addEventListener('scroll', _.throttle(function () {
-  // 페이지 스크롤 위치가 500px이 넘으면.
+  console.log(window.scrollY);
   if (window.scrollY > 500) {
-    // Badge 요소 숨기기!
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션);
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
-    })
-    // 상단으로 스크롤 버튼 보이기!
+    });
+    // 버튼 보이기
     gsap.to(toTopEl, .2, {
       x: 0
-    })
-
-  // 페이지 스크롤 위치가 500px이 넘지 않으면.
+    });
   } else {
-    // Badge 요소 보이기!
+    // 배지 보이기
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
-    })
-    // 상단으로 스크롤 버튼 숨기기!
+    });
     gsap.to(toTopEl, .2, {
       x: 100
-    })
-  }
+    });
+  };
 }, 300))
-// 상단으로 스크롤 버튼을 클릭하면,
 toTopEl.addEventListener('click', function () {
-  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
   gsap.to(window, .7, {
     scrollTo: 0
-  })
-})
+  });
+});
 
 
 /**
  * 순서대로 나타나는 기능
  */
-// 나타날 요소들(.fade-in) 찾기.
-const fadeEls = document.querySelectorAll('.visual .fade-in')
-// 나타날 요소들을 하나씩 반복해서 처리!
+const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
-  // 각 요소들을 순서대로(delay) 보여지게 함!
   gsap.to(fadeEl, 1, {
     delay: (index + 1) * .7,
     opacity: 1
-  })
-})
+  });
+});
 
 
 /**
@@ -124,9 +88,9 @@ new Swiper('.awards .swiper-container', {
  * Promotion 슬라이드 토글 기능
  */
 // 슬라이드 영역 요소 검색!
-const promotionEl = document.querySelector('.promotion')
+const promotionEl = document.querySelector('.promotion');
 // 슬라이드 영역를 토글하는 버튼 검색!
-const promotionToggleBtn = document.querySelector('.toggle-promotion')
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
 // 슬라이드 영역 숨김 여부 기본값!
 let isHidePromotion = false
 // 토글 버튼을 클릭하면,
@@ -135,12 +99,12 @@ promotionToggleBtn.addEventListener('click', function () {
   isHidePromotion = !isHidePromotion
   // 요소를 숨겨야 하면,
   if (isHidePromotion) {
-    promotionEl.classList.add('hide')
+    promotionEl.classList.add('hide');
   // 요소가 보여야 하면,
   } else {
-    promotionEl.classList.remove('hide')
-  }
-})
+    promotionEl.classList.remove('hide');
+  };
+});
 
 
 /**
@@ -166,30 +130,23 @@ function floatingObject(selector, delay, size) {
     }
   )
 }
-floatingObject('.floating1', 1, 15)
-floatingObject('.floating2', .5, 15)
-floatingObject('.floating3', 1.5, 20)
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
 
 
 /**
  * 요소가 화면에 보여짐 여부에 따른 요소 관리
  */
 // 관리할 요소들 검색!
-const spyEls = document.querySelectorAll('section.scroll-spy')
+const spyEls = document.querySelectorAll('section.scroll-spy');
 // 요소들 반복 처리!
 spyEls.forEach(function (spyEl) {
   new ScrollMagic
     .Scene({ // 감시할 장면(Scene)을 추가
       triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
       triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
-    })
+    });
     .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
-})
-
-
-/**
- * 올해가 몇 년도인지 계산
- */
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear()
+});
